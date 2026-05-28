e3a854ff330b26ce4fd35b1dcbcce84c8c3fad7d
Piyush467 <piyushmehrotra61@gmail.com>
2026-05-28 23:17:28 +0530

    fixed cors error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json()); // middleware that allow reading of data from req.body()
 
-app.use(cookieParser())
-app.use(cors({
-    origin: "https://hire-nova-ten.vercel.app/",
-    credentials: true
-}))
-
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://hire-nova-ten.vercel.app",
+    credentials: true,
+  }),
+);
 
 /* require all routes here*/
-const authRouter = require('./routes/auth.routes')
-const interviewRouter = require("./routes/interview.routes")
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
 
 /* using all routes here */
-app.use("/api/auth", authRouter)
-app.use("/api/interview", interviewRouter)
-
-
+app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter);
 
 module.exports = app;
